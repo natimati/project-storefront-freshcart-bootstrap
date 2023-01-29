47acb64d8e98940ef445e3ce28ab927d57bed58e
natimati <natmateusiak@gmail.com>
2023-01-29 17:53:35 +0100

    feat: two categories products sections added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, map, shareReplay } from 'rxjs';
+import { ProductModel } from 'src/app/models/product.model';
+import { CategoryProductsQueryModel } from 'src/app/query-models/category-products.query-model';
+import { ProductsService } from 'src/app/services/products.service';
 import { CategoryModel } from '../../models/category.model';
 import { StoreModel } from '../../models/store.model';
 import { CategoriesService } from '../../services/categories.service';
 import { StoresService } from '../../services/stores.service';
+
 
 @Component({
   selector: 'app-home',
@@ -12,10 +16,34 @@ import { StoresService } from '../../services/stores.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories().pipe(shareReplay(1));
   readonly storesList$: Observable<StoreModel[]> = this._storesService.getAllStores();
+  readonly productsList$: Observable<ProductModel[]> = this._productsService.getAllProducts();
 
-  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService) {
+  readonly categoriesProducts$: Observable<CategoryProductsQueryModel[]> = combineLatest([
+    this.categoriesList$,
+    this.productsList$
+  ]).pipe(map(([categories, products]) => {
+    const fruitsCategory = categories.find(category => category.name === 'Fruits & Vegetables');
+    const snacksCategory = categories.find(category => category.name === 'Snack & Munchies');
+    const categoriesProducts = products.reduce((acc: CategoryProductsQueryModel[], product) => {
+      if (fruitsCategory && product.categoryId === fruitsCategory.id) {
+        acc[0].products.push(product)
+      } else if (snacksCategory && product.categoryId === snacksCategory.id) {
+        acc[1].products.push(product)
+      }
+      return acc
+    }, [{ categoryName: 'Fruits & Vegetables', products: [] }, { categoryName: 'Snack & Munchies', products: [] }]) 
+    return categoriesProducts.map(category => ({
+      categoryName: category.categoryName,
+      products: category.products.sort((a, b) => {
+        return b.featureValue - a.featureValue
+      }).slice(0,5)
+    }
+    ))
+  }))
+
+  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService, private _productsService: ProductsService) {
   }
 
   convertDistance(distance: number): number {
