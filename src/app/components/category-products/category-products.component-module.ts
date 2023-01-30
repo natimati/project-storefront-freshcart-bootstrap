7097ca2b26124be35dfcd21b3bd2dcdbde111d26
natimati <natmateusiak@gmail.com>
2023-01-30 16:54:22 +0100

    feat: categories list and detail simple card showed

--- src/app/components/category-products/category-products.component-module.ts
+++ src/app/components/category-products/category-products.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { CategoryProductsComponent } from './category-products.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, RouterModule],
   declarations: [CategoryProductsComponent],
   providers: [],
   exports: [CategoryProductsComponent]
--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
+import { CategoryModel } from '../../models/category.model';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-category-products',
@@ -7,4 +11,14 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryProductsComponent {
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly currentCategoryId$: Observable<string> = this._activatedRoute.params.pipe(
+    map(params => params['categoryId'].toString())
+  );
+  readonly currentCategory$: Observable<CategoryModel> = this.currentCategoryId$.pipe(
+    switchMap(id => this._categoriesService.getCategoryById(id))
+  )
+
+  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute) {
+  }
 }
